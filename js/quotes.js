152effd4344c2d2eaeaf8498c690de452ef96dbb
plottwisters <45325736+plottwisters@users.noreply.github.com>
2019-04-16 23:57:31 -0700

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,59 @@
+var quotes = [
+  {
+    quote: "Find out who you are and do it on purpose.",
+    person: "Dolly Parton"
+  },
+  {
+    quote: "Without knowledge, action is useless and knowledge without action is futile.",
+    person: "Abu Bakr"
+  },
+  {
+    quote: "School is the beginning of the child's life work",
+    person: "Eleanor Roosevelt"
+  },
+  {
+    quote: "Part of the education of every child to show it as many as possible of the various types of lives which make up the world around it.",
+    person: "Eleanor Roosevelt"
+  },
+  {
+    quote: "Not everyone can become a great artist, but a great artist can come from anywhere.",
+    person: "Eleanor Roosevelt"
+  },
+  {
+    quote: "Fantasy. Lunacy. All revolutions are, until they happen, then they are historical inevitabilities.",
+    person: "David Mitchell"
+  },
+  {
+    quote: "Everything you make returns to the Earth as food or poison.",
+    person: "Slow Factory"
+  },
+  {
+    quote: "Shallow men believe in luck and circumstance. Strong men believe in cause and effect.",
+    person: "Ralph Waldo Emerson"
+  },
+  {
+    quote: "The ultimate, hidden truth of the world is that it is something that we make, and could just as easily make differently.",
+    person: "David Graeber"
+  },
+  {
+    quote: "The world is full of magic things, patiently waiting for our senses to grow sharper!",
+    person: "William Butler Yeats"
+  }
+];
+
+$(document).ready(function() {
+
+  var wiseWords = quotes[Math.floor(Math.random()*quotes.length)];
+  var quote = wiseWords.quote;
+  var person = wiseWords.person;
+  $("#quote").html(quote);
+  $("#person").html(person);
+
+  $(".refresh").click(function() {
+      var wiseWords = quotes[Math.floor(Math.random()*quotes.length)];
+      var quote = wiseWords.quote;
+      var person = wiseWords.person;
+      $("#quote").html(quote);
+      $("#person").html(person);
+  });
+});
