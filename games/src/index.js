6e08fe44bed7284d36b4c24afd40838f953420be
AdityaAgg <agga140@usc.edu>
2019-06-21 17:11:29 -0700

    preparation for deployment

--- games/src/index.js
+++ games/src/index.js
@@ -58,7 +58,7 @@ class App extends Component {
     //for "/" show variable above with links for all other urls include only routes
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div>
             <Route component={gameLinksContainer}/>
 
