7d42f2ee64742085b448bae9217289aceccdcd0a
AdityaAgg <agga140@usc.edu>
2019-07-26 16:10:29 -0700

    dependency vulnerability removal

--- games/install/prod_deploy.sh
+++ games/install/prod_deploy.sh
@@ -1,3 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf $DIR/../dist/
 parcel build $DIR/../src/index.html $DIR/../src/assets/styles/*  $DIR/../src/assets/img/* --out-dir DIR/../dist/ --public-url '/games/dist'
+rm $DIR/../dist/*.map
