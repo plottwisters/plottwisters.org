ee6d694d6df2fde1e00ceea75cbf177b22428965
Jenny Liu Zhang <jenliuzhang@gmail.com>
2020-09-06 00:50:34 -0700

    new site who dis 2

--- archive/games/src/redux/actions/tbos/index.js
+++
@@ -1,5 +0,0 @@
-import { ActionCreators as UndoActionCreators } from 'redux-undo';
-export * from './root';
-export * from './task';
-
-export let {redo, undo, jumpToFuture, jumpToPast} = UndoActionCreators;
--- archive/games/src/redux/actions/trail_viewer.js
+++
@@ -1,9 +0,0 @@
-import {ActionType} from './tbos/action_type';
-
-
-export function toggleCookieTrail(trail) {
-  return {
-    type: ActionType.TOGGLE_TRAIL,
-    trail
-  }
-}
